a7643d7dc39c77b6428ffd21cd15be5841e2767d
Nathalia Gutierrez Pinilla <156914349+Natha91-z@users.noreply.github.com>
2024-06-10 04:47:32 -0500

    Explicación de Contructores

--- Aritmetica.java
+++ Aritmetica.java
@@ -2,6 +2,12 @@ public class Aritmetica {
     int operando1;
     int operando2;
 
+    // Constructor
+    public  Aritmetica(int op1, int op2){
+        operando1 =op1;
+        operando2 =op2;
+    }
+
     void sumar(){
         int resultado = operando1 + operando2;
         System.out.println("Resultado Suma: " +resultado);
@@ -13,14 +19,10 @@ public class Aritmetica {
     }
     public static void main(String[] args){
         System.out.println("*** Ejemplo Aritmetico***");
-        var aritmetica1 = new Aritmetica();
-        aritmetica1.operando1=5;
-        aritmetica1.operando2=7;
+        var aritmetica1 = new Aritmetica(5,7);
+//        aritmetica1.operando1=5;
+//        aritmetica1.operando2=7;
         aritmetica1.sumar();
-
-        var aritmetica2 = new Aritmetica();
-        aritmetica2.operando1=19;
-        aritmetica2.operando2=7;
-        aritmetica2.restar();
+        aritmetica1.restar();
     }
 }
