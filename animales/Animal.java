133d52217473f5282e1eb75e1f602b2f472ba9e6
Nathalia Gutierrez Pinilla <156914349+Natha91-z@users.noreply.github.com>
2024-06-11 02:33:55 -0500

    Polimorfismo
    Ejemplo de como se aplica el polimorfismo con unm ejemplo de amimales, en el cual dos animales ejecutan un proceso pero se presenta primero de la herencia y  se aplica la ejecucion de cada un sonido, dependiendo el tipo de animal.

--- animales/Animal.java
+++ animales/Animal.java
@@ -1,11 +1,9 @@
 package animales;
 
 public class Animal {
-    public void comer(){
-        System.out.println("Como muchas veces al dia");
-    }
-    public void dormir(){
-        System.out.println("Duermo muchas veces al dia");
+    public void hacerSonido(){
+        System.out.println("El animal hace un sonido");
+
     }
 
 }// fin clase animal
@@ -13,29 +11,23 @@ public class Animal {
 // clase que hereda
 
 class Perro extends Animal{
+    @Override
     public void hacerSonido(){
-        System.out.println("Puedo ladrar");
+        System.out.println("El perro hace wauf");
     }
-    @Override // significa sobre escribir en el codigo
-    public void dormir(){
-        System.out.println("Duermo 15 horas al dia");
-        System.out.println("Metodo clase padre: ");
-        super.dormir(); // nos permite llamar y aplicar de nuevo la clase padre.
+}
+
+class Gato extends Animal{
+    @Override
+    public void hacerSonido(){
+        System.out.println("El gato hace miau");
     }
 }
 
 class PruebaAnimal{
-    public static void main(String[] args){
-        System.out.println("*** Ejemplo Herencia***");
-        System.out.println("Clase Padre, soy un Animal");
-        var animal1 = new Animal();
-        animal1.comer();
-        animal1.dormir();
-
-        System.out.println("\nClase Hija, soy un perro");
-        var perro1 = new Perro();
-        perro1.comer();
-        perro1.dormir();
-        perro1.hacerSonido();
+    public static void main(String[] args) {
+        System.out.println("*** Ejemplo Polimorfismo ***");
+        Animal animal = new Gato(); // polimorfismo
+        animal.hacerSonido();
     }
 }
